9e511be9d65b9f43d2df2ca46a6e45ab387d5207
Marionna <marionna@Marionnas-MacBook-Air-719.local>
2025-11-10 10:05:01 -0500

    Add filtering tags logic

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,13 @@
 class Event < ApplicationRecord
   validates :title, presence: true
-  validates :date,  presence: true
+  validates :date, presence: true
   has_one_attached :image
+  
+  before_validation :ensure_tags_is_array
+  
+  private
+  
+  def ensure_tags_is_array
+    self.tags = [] if tags.nil?
+  end
 end
